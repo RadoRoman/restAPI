08485e6c3899cd069f638233c043d4bc225491c7
RadoRoman <20imc10324@fh-krems.ac.at>
2022-04-14 23:05:12 +0200

    api models created, get & post method added

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from models import User, Role
+from typing import List
 
 app = FastAPI()
+
+db: List[User] = []
+
 
 
 @app.get('/')
 def root():
     return {"Hi": "World, Im here"}
+
+@app.get('/api/users')
+async def get_users():
+    return db
+
+
+@app.post('/api/post_user')
+async def register_user(user: User):
+    db.append(user)
+    return {"username": user.username}
---
+++ models.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from enum import Enum
+from typing import List
+
+class Role(str, Enum):
+    admin = "admin"
+    secretary = "secretary"
+    manager = "manager"
+
+
+class User(BaseModel):
+    user_name: str
+    password: str
+    roles: List[Role]
